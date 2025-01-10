7764c8574414383063e253463394599f714a29fd
tranthanhtan2203 <laodaicaydiep@gmail.com>
2025-01-10 10:23:23 +0700

    lancuoi

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :adms
-  root to: "home#homepage"
+  # root to: "home#homepage"
+  root "products#index"
   resource :session
   resources :passwords, param: :token
   resources :products do
     resources :subscribers, only: [ :create ]
   end
 
-  root "products#index"
+  
 end
